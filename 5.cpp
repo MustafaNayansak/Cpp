8f583d475c4e9e2b78670271ca8476814eea4935
Mustafa Nayansak <32681985+MustafaNayansak@users.noreply.github.com>
2019-06-05 23:24:14 +0300

    IF fonksiyon yapısı

---
+++ 5.cpp
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h> 
+
+int main (int argc, char *argv[])
+{
+    int sayi;
+    printf(" sayi giriniz :");
+    scanf("%d",&sayi);
+    
+    if(sayi<10)
+    {
+               printf("sayi 10dan kucuktur");
+    }
+    
+    system("pause");
+    return 0;
+}           
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
