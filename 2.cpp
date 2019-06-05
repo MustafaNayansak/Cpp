e029e9e1fb1e4f66d77936d7f46ef1c1f29c2027
Mustafa Nayansak <32681985+MustafaNayansak@users.noreply.github.com>
2019-06-05 23:21:32 +0300

    Çemberin Çap-Çevre-Alan Hesaplaması

---
+++ 2.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h> 
+
+int main (int argc, char *argv[])
+{
+    float r,cap,cevre,alan;
+    float r;  //yarýçap
+    printf("Yaricap degeri giriniz:");
+    scanf("%f",&r);
+    cap=2*r
+    printf("Cemberin capi = %.2f\n",2*r);
+    cevre=2*3.14159;
+    printf("Cemberin cevresi = %.2f\n",2*3.14159*r);
+    alan=3.14159*r*r;
+    printf("Cemberin alani = %.2f\n",3.14159*r*r);
+    
+    system("PAUSE");
+    return 0;
+}
