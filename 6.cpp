fa51b06118efe6f6965469ae9d88859550116636
Mustafa Nayansak <32681985+MustafaNayansak@users.noreply.github.com>
2019-06-05 23:25:13 +0300

    Girilen Sayılardan Büyük Olanı Hesaplama

---
+++ 6.cpp
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h> 
+
+int main (int argc, char *argv[])
+{
+    int num1,num2;
+    printf("İki sayi giriniz:\n");
+    scanf("%d%d",&num1,&num2);
+    
+    if(num1==num2)
+    {
+                  printf("%d esittir %d\n",num1,num2);
+    }
+      if(num1==num2)
+    {
+                  printf("%d esittir %d\n",num1,num2);
+    }
+     if(num1!=num2)
+    {
+                  printf("%d esit degildir %d\n",num1,num2);
+    }
+    if(num1<num2)
+    {
+                  printf("%d kucuktur %d\n",num1,num2);
+    }
+    if(num1>num2)
+    {
+                  printf("%d buyuktur %d\n",num1,num2);
+    }
+    if(num1<=num2)
+    {
+                  printf("%d kucuk esittir %d\n",num1,num2);
+    }            
+    if(num1>=num2)
+    {
+                  printf("%d buyuk esittir %d\n",num1,num2);
+    }
+    
+    
+    //sen ayarlarsın eşitlikleri kaldeş
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    system("pause");
+    return 0;
+}           
