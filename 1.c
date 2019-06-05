e8d8b7ddd7a2d78fcebc4b81847e97ac4be83b05
Mustafa Nayansak <32681985+MustafaNayansak@users.noreply.github.com>
2019-06-05 23:20:41 +0300

    Nam-ı Diğer Hello World

---
+++ 1.c
@@ -0,0 +1,20 @@
+//her bir kütüphanenin içinde hazýr yazýlmýþ fonksiyonlar var 
+//stdio.h fonksiyonunu indirirsek içindeki fonksiyonlarý kullanabilirsin
+
+#include<stdio.h>
+main()
+{
+      printf("Hello World\n");
+      //Çalýþtýrýlan programýn ekranda kalmasý için aþaðýdaki komut yazýlýr
+      system("pause");
+}
+
+//Kütüphaneler # komutuyla geliyor, tüm kütüphaneler .h uzantýsýyla olur
+//C anahtar kelimeleri kalýn siyah þeklinde yazar
+//Birþeyi kaydetmek istediðimizde deðiþkenleri kullanýyoruz. Özellikler;
+//1. Hiçbir deðiþkenin baþýnda sayý veya _ olamaz
+//2. Deðiþken isimleri harfin büyük küçük olmasýna duyarlý
+//3. Deðiþkeni kullanmadan önce tanýmlaman gerekiyor
+//4. Deðiþken tipleri; char-karakter, int-tamsayý, float,double-kesiklisayý
+//5. Bir karaktere deðer atarken tek týrnak kullanýlýr
+//6. Kesirli sayýlar noktayla yazýlýr
